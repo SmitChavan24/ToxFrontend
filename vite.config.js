4472468ddc54fbf5820e093333121c6bb2d1ecc0
unknown <smitchavan42@gmail.com>
2025-07-23 05:17:23 +0530

    persisting site

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,15 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react(),],
+  server: {
+    historyApiFallback: true
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   build: {
-    target: "ES2022" // <--------- ✅✅✅✅✅✅
+    target: "ES2022"
   },
 })
